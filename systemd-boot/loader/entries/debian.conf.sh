fda2743f7ae773fbcdc5b952e2a4d360d6285eb4
Yang Zhang <CoderOJ@outlook.com>
2026-03-23 02:25:11 +0800

    upd(systemd-boot): update console options in debian.conf.sh

--- systemd-boot/loader/entries/debian.conf.sh
+++ systemd-boot/loader/entries/debian.conf.sh
@@ -4,5 +4,5 @@ cat - << EOF
 title   Debian ${DEBIAN_VERSION:-trixie}
 linux   $(basename $(readlink ./mnt/vmlinuz))
 initrd  $(basename $(readlink ./mnt/initrd.img))
-options root=UUID=$(findmnt -no UUID ./mnt) rw console=ttyS0,115200 earlyprintk=ttyS0,115200
+options root=UUID=$(findmnt -no UUID ./mnt) rw console=tty0 console=ttyS0,115200 earlycon=uart,io,0x3f8,115200
 EOF
